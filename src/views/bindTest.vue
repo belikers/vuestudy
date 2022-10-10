7cb662df972bdef6c5db0a0d8dcb678637efcbdf
remohjh <1251683316@qq.com>
2022-10-10 08:42:41 +0800

    test

---
+++ src/views/bindTest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>姓：<input type="text" v-model="per.surname" /></div>
+    <div>名：<input type="text" v-model="per.name" /></div>
+    <div>姓名：<input type="text" v-model="per.fullName" /></div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from "vue";
+
+export default {
+  setup() {
+    let per = reactive({
+      surname: "勇敢",
+      name: "小陈",
+    });
+    per.fullName = computed({
+      get() {
+        return per.surname + "~" + per.name;
+      },
+      set(value) {
+        var arr = value.split("~");
+        per.surname = arr[0];
+        per.name = arr[1];
+      },
+    });
+    return {
+      per,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/vFor.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="(item, index) of fruit" :key="index">
+      索引值 index:{{ index }}---item:{{ item }} 每一项name:{{
+        item.name
+      }}
+      每一项color:{{ item.color }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "vFor",
+  data() {
+    return {
+      fruit: [
+        { name: "苹果", color: "红色" },
+        { name: "梨子", color: "黄色" },
+        { name: "香蕉", color: "橘色" },
+        { name: "橘子", color: "绿色" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
